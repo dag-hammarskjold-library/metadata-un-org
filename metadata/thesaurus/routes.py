c1bd9428a19b764b68602670542909a69b5e23e0
aaronhelton <abhelton@gmail.com>
2018-11-09 20:12:16 +0000

    connect to PoolParty APIs

--- metadata/thesaurus/routes.py
+++ metadata/thesaurus/routes.py
@@ -1,10 +1,11 @@
 from flask import render_template, redirect, url_for, request, jsonify, abort
+from metadata.config import API
 from metadata.semantic import Term
 from metadata.thesaurus import thesaurus_app
 from metadata.thesaurus.config import INIT, LIST_CLASSES, SINGLE_CLASSES, LANGUAGES, KWARGS
 from metadata.config import GLOBAL_KWARGS, GRAPH
 from metadata.utils import get_preferred_language
-import re
+import re, requests, json
 
 # Common set of kwargs to return in all cases. 
 return_kwargs = {
@@ -22,6 +23,46 @@ def index():
     get_preferred_language(request, return_kwargs)
     return render_template('thesaurus_index.html', **return_kwargs)
 
+@thesaurus_app.route('/<id>')
+def get_by_id(id):
+    '''
+    This should return the landing page for a single instance of
+    a record, such as an individual Concept.
+
+    Positive matching is done against a whitelist of RDF Types 
+    and id regular expressions patterns as definied in 
+    metadata.thesaurus.config. This is intended to pre-screen 
+    user input and reject anything that doesn't fit a strict 
+    pattern.
+    '''
+    get_preferred_language(request, return_kwargs)
+    for single_class in SINGLE_CLASSES:
+        this_sc = SINGLE_CLASSES[single_class]
+        p = re.compile(this_sc['id_regex'])
+        if p.match(id):
+            #print(this_sc)
+            uri = INIT['uri_base'] + id
+            return_properties = ",".join(this_sc['properties'])
+            api_path = '%s%s/concept?concept=%s&properties=%s&language=%s' % (
+                API['source'], INIT['thesaurus_pattern'], uri, return_properties, return_kwargs['lang']
+            )
+            #print(api_path)
+            breadcrumbs = build_breadcrumbs(uri)
+            jsresponse = requests.get(api_path, auth=(API['user'],API['password']))
+            if jsresponse.status_code == 200:
+                jsdata = json.loads(jsresponse.text)
+                for lst in this_sc['lists']:
+                    try:
+                        jsdata[lst] = build_list(jsdata[lst],'prefLabel')
+                    except KeyError:
+                        pass
+                return render_template(this_sc['template'], **return_kwargs, data=jsdata, bcdata=breadcrumbs)
+            else:
+                abort(404)
+        else:
+            next
+    abort(404)
+
 @thesaurus_app.route('/browse/<list_class>')
 def browse(list_class):
     '''
@@ -36,32 +77,34 @@ def browse(list_class):
     else:
         abort(403)
 
-@thesaurus_app.route('/<single_class>/<id>')
-def single_class(single_class,id):
-    '''
-    This should return the landing page for a single instance of
-    a record, such as an individual Concept.
-
-    Positive matching is done against a whitelist of RDF Types 
-    and id regular expressions patterns as definied in 
-    metadata.thesaurus.config. This is intended to pre-screen 
-    user input and reject anything that doesn't fit a strict 
-    pattern.
-    '''
-    get_preferred_language(request, return_kwargs)
-    if single_class in SINGLE_CLASSES:
-        this_sc = SINGLE_CLASSES[single_class]
-        p = re.compile(this_sc['id_regex'])
-        if p.match(id):
-            # the pattern matches at least
-            uri = str(INIT['uri_base'] + id)
-            term = Term(uri, 'definition')
-            return render_template(this_sc['template'], **return_kwargs)
-        else:
-            abort(403)
-    else:
-        abort(403)
-
 @thesaurus_app.route('/search')
 def search():
     return render_template('search.html', **return_kwargs)
+
+def build_breadcrumbs(uri):
+    api_path = '%s%s/paths?concept=%s&language=%s' % (
+        API['source'], INIT['thesaurus_pattern'], uri, return_kwargs['lang']
+    )
+    print(api_path)
+    jsresponse = requests.get(api_path, auth=(API['user'],API['password']))
+    if jsresponse.status_code == 200:
+        bcdata = json.loads(jsresponse.text)
+        return bcdata
+    else:
+        return None
+
+def build_list(concepts, sort_key):
+    '''
+    This takes a list of URIs and returns a list of uri,label tuples sorted by the label
+    in the selected language.
+    '''
+    api_path = '%s%s/concepts?concepts=%s&language=%s' %(
+        API['source'], INIT['thesaurus_pattern'], ",".join(concepts), return_kwargs['lang']
+    )
+    jsresponse = requests.get(api_path, auth=(API['user'],API['password']))
+    if jsresponse.status_code == 200:
+        jsdata = json.loads(jsresponse.text)
+        sorted_js = sorted(jsdata, key=lambda k: k[sort_key])
+        return sorted_js
+    else:
+        return None
