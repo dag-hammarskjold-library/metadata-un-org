5122852eb45e5c9273da98802a1183fea106381e
aaronhelton <abhelton@gmail.com>
2019-04-11 13:51:19 +0000

    update requirements

--- metadata/cache.py
+++ metadata/cache.py
@@ -1,6 +1,15 @@
 from flask_caching import Cache
 
+# memcached
 cache = Cache(config={
     'CACHE_TYPE': 'memcached',
     'CACHE_DEFAULT_TIMEOUT': 0
 })
+
+# Redis
+#cache = Cache(config={
+#    'CACHE_TYPE': 'redis',
+#    'CACHE_DEFAULT_TIMEOUT': 0,
+#    'CACHE_REDIS_HOST': '',
+#    'CACHE_REDIS_PORT': 6379,
+#})
