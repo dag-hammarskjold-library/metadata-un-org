6398c7a152f6b043ce59baf619b9473505d3e85f
aaronhelton <abhelton@gmail.com>
2019-08-07 17:42:03 +0000

    basic lookup by tcode

--- metadata/thesaurus/routes.py
+++ metadata/thesaurus/routes.py
@@ -52,7 +52,11 @@ def index():
 
 @thesaurus_app.route('/T<id>')
 def get_by_tcode(id):
-    return jsonify({"Got id": "T" + id})
+    this_id = "T" + id
+    dsl_q = '''{ "query": { "match": { "tcode": "%s" } } }''' % this_id
+				
+    this_uri = ES_CON.search(index=CONFIG.INDEX_NAME, body=dsl_q)['hits']['hits'][0]['_source']['uri']
+    return jsonify({"id": this_id, "uri": this_uri})
 
 @thesaurus_app.route('/<id>')
 @cache.cached(timeout=None, key_prefix=make_cache_key)
