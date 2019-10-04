07ed4736e9d4711374e4ef2df0258a6f26637895
aaronhelton <abhelton@gmail.com>
2019-10-04 14:50:48 -0400

    once more

--- metadata/sdg/__init__.py
+++ metadata/sdg/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from metadata.sdg.config import CONFIG
 
-sdg_app = Blueprint('sdgs', __name__, template_folder='templates', static_folder='static')
+sdg_app = Blueprint(CONFIG.INIT['blueprint_name'], __name__, template_folder='templates', static_folder='static')
 
 from metadata.sdg import routes
