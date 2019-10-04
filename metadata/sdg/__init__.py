fb9d7cc8ce9b5cb3cd58a24e4571716a78f66bce
aaronhelton <abhelton@gmail.com>
2019-10-04 14:49:18 -0400

    oops change that back

--- metadata/sdg/__init__.py
+++ metadata/sdg/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-#from metadata.sdg.config import CONFIG
+from metadata.sdg.config import CONFIG
 
 sdg_app = Blueprint('sdgs', __name__, template_folder='templates', static_folder='static')
 
