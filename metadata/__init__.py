588e725de05b41b6b517976eef76d7d6b66de31b
Aaron Helton <aaronhelton@Aarons-MacBook-Pro.local>
2019-10-07 10:26:21 -0400

    zappafy this thing

--- metadata/__init__.py
+++ metadata/__init__.py
@@ -5,15 +5,18 @@ from .config import GLOBAL_CONFIG
 
 LANGUAGES = GLOBAL_CONFIG.LANGUAGES
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+print(app)
 babel = Babel(app)
 cache.init_app(app)
 
 @babel.localeselector
 def get_locale():
     locale = request.args.get('lang',request.accept_languages.best_match(LANGUAGES.keys()))
-    print(locale)
+    #print(locale)
     return locale
+
+from metadata.routes import *
 
 # Use this section to register your sub-applications.
 from metadata.thesaurus import thesaurus_app
@@ -23,4 +26,4 @@ from metadata.sdg import sdg_app
 from metadata.sdg.config import CONFIG as sdg_config
 app.register_blueprint(sdg_app, url_prefix='/kos/sdg', title=sdg_config.INIT['title'])
 
-from metadata import routes
+
