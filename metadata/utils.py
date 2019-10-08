ba47f57c492adefe537f01c5dc67ab62c649909b
Aaron Helton <aaronhelton@Aarons-MacBook-Pro.local>
2019-10-08 09:34:24 -0400

    no need to print search results in log

--- metadata/utils.py
+++ metadata/utils.py
@@ -59,7 +59,7 @@ def query_es(connection, index_name, query, lang, max_hits):
 
     #print(dsl_q)
     match = connection.search(index=index_name, body=dsl_q, size=max_hits)
-    pp = pprint.PrettyPrinter(indent=2)
+    #pp = pprint.PrettyPrinter(indent=2)
     #pp.pprint(match)
     return match
 
