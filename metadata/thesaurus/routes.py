c2b308a7e2ed5fc5b7e7dc453ec4e7f9b822f6e4
Aaron Helton <aaronhelton@Aarons-MacBook-Pro.local>
2019-11-25 14:35:34 -0500

    set return_kwargs

--- metadata/thesaurus/routes.py
+++ metadata/thesaurus/routes.py
@@ -384,7 +384,7 @@ def search():
 
     #print(pagination.page, page)
 
-    return render_template('thesaurus_search.html', return_kwargs, results=response, query=query, count=count, lang=preferred_language, subtitle=gettext('Search'))
+    return render_template('thesaurus_search.html', results=response, query=query, count=count, lang=preferred_language, subtitle=gettext('Search'), return_kwargs=return_kwargs)
 
 @thesaurus_app.route('/autocomplete', methods=['GET'])
 def autocomplete():
