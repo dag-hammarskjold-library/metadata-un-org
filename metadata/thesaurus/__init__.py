e84c70bafbe3988e2ee90669b4527e60a6bc82fe
Aaron Helton <mariusagricola@gmail.com>
2021-03-05 15:47:06 -0500

    update requirements and start commands

--- metadata/thesaurus/__init__.py
+++ metadata/thesaurus/__init__.py
@@ -1,5 +1,12 @@
 from flask import Blueprint
+import click
 
-thesaurus_app = Blueprint('thesaurus', __name__, template_folder='templates', static_folder='static')
+thesaurus_app = Blueprint('thesaurus', __name__, template_folder='templates', static_folder='static', cli_group='thesaurus')
+
+from metadata.lib.ppmdb import *
+@thesaurus_app.cli.command('foo')
+def foo():
+    print("bar")
+
 
 from metadata.thesaurus import routes
