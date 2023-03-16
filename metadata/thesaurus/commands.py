a7f973f225f4e991a315119005966dae8b55012c
Aaron Helton <helton@un.org>
2023-03-16 16:27:24 -0400

    rewrite es indexing script

--- metadata/thesaurus/commands.py
+++ metadata/thesaurus/commands.py
@@ -26,26 +26,28 @@ def rebuild_index(datafile):
     from tqdm import tqdm
     import importlib, json, requests
     import ssl
-    from elasticsearch.connection import create_ssl_context
+    #from elasticsearch.connection import create_ssl_context
 
-    context = create_ssl_context()
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
+    #context = create_ssl_context()
+    #context = ssl.create_default_context()
+    #context.check_hostname = False
+    #context.verify_mode = ssl.CERT_NONE
     
     es_uri = CONFIG.ELASTICSEARCH_URI
-    es_con = Elasticsearch(es_uri, ssl_context=context)
+    #es_con = Elasticsearch(es_uri, ssl_context=context)
+    es_con = Elasticsearch(es_uri)
     index_name = CONFIG.INDEX_NAME
 
     if not os.path.exists(datafile):
         raise "file not found"
 
     print(f"Deleting {index_name}")
-    if es_con.indices.exists(index_name):
-        res = es_con.indices.delete(index_name)
+    if es_con.indices.exists(index=index_name):
+        res = es_con.indices.delete(index=index_name)
 
     print(f"Creating {index_name}")
     body = CONFIG.THESAURUS_INDEX
-    res = es_con.indices.create(index_name, body=body)
+    res = es_con.indices.create(index=index_name, body=body)
 
     print(f"Preparing mapping")
     mapping = CONFIG.MAPPING
@@ -87,35 +89,28 @@ SELECT * WHERE {
 }"""
 
     #for uri, tcode in tqdm(graph.query(querystring)):
-    for uri, tcode in graph.query(querystring):
+    for tcode, uri in graph.query(querystring):
         this_uri = uri
         this_tcode = tcode
-        print(this_uri, this_tcode)
+        print('Processing new term:')
+        print(f'uri: {this_uri}, tcode: {this_tcode}')
         doc = {"uri": this_uri}
         for lang in CONFIG.LANGUAGES:
             pref_labels = []
-            for label in graph.preferredLabel(URIRef(this_uri), lang):
-                #print(label)
-                pref_labels.append(label[1])
+            pref_labels = [o for s,p,o in graph.triples((URIRef(this_uri), SKOS.prefLabel, None)) if o.language == lang]
             doc.update({"labels_{}".format(lang): pref_labels})
 
-            alt_labels = []
-            for label in graph.objects(URIRef(this_uri), SKOS.altLabel):
-                if label.language == lang:
-                    alt_labels.append(label)
+            alt_labels = [o for s,p,o in graph.triples((URIRef(this_uri), SKOS.altLabel, None)) if o.language == lang]
             doc.update({"alt_labels_{}".format(lang): alt_labels})
 
             if tcode is not None:
                 doc.update({"tcode": this_tcode}) 
 
-            hidden_labels = []
-            for label in graph.objects(URIRef(this_uri), SKOS.hiddenLabel):
-                if label.language == lang:
-                    hidden_labels.append(label)
+            hidden_labels = [o for s,p,o in graph.triples((None, SKOS.hiddenLabel, None)) if o.language == lang]
             doc.update({"hidden_labels_{}".format(lang): hidden_labels})
 
             payload = json.dumps(doc)
-            print(payload)
+            print(f'writing: {payload}')
 
             res = es_con.index(index=index_name, body=payload)
             doc = {"uri": this_uri}
